d237fd1b122c9343bea88168cf5e43c719d9f0f9
Brandon Thomas <brandon@serverstage.net>
2019-01-01 21:02:58 +0100

    n2

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -14,12 +14,12 @@ main() {
   echo "$STORAGE" > storage.json
   
   echo -e '[LiveSwift]' >> rclone.config
-  echo $VCAP_SERVICES | ./jq --raw-output 'foreach ."user-provided"[].credentials as $swift (1; {type:"swift", user:$swift.OS_USERNAME, key:$swift.OS_PASSWORD, auth:$swift.OS_AUTH_URL,tenant_id:$swift.OS_PROJECT_ID,domain:env.SWIFTDOMAIN,storage_url:env.SWIFTSTORAGEURL}) | to_entries | map("\(.key)=\(.value)") | .[]' >> rclone.config
+  echo $VCAP_SERVICES | ./jq --raw-output 'foreach ."user-provided"[].credentials as $swift (1; {type:"swift", user:$swift.OS_USERNAME, key:$swift.OS_PASSWORD, auth:$swift.OS_AUTH_URL, tenant_id:$swift.OS_PROJECT_ID, domain:$ENV.SWIFTDOMAIN, storage_url:$ENV.SWIFTSTORAGEURL}) | to_entries | map("\(.key)=\(.value)") | .[]' >> rclone.config
   
   echo -e '[Backup]' >> rclone.config
-  echo $STORAGE | ./jq --raw-output 'foreach ."storage"[0] as $swift (1; {type:"swift",user:$swift.username,key:$swift.password,auth:$swift.auth_url,tenant_id:$swift.project_id,domain:env.SWIFTDOMAIN,storage_url:env.SWIFTSTORAGEURL }) | to_entries | map("\(.key)=\(.value)") | .[]' >> rclone.config
+  echo $STORAGE | ./jq --raw-output 'foreach ."storage"[0] as $swift (1; {type:"swift", user:$swift.username, key:$swift.password, auth:$swift.auth_url, tenant_id:$swift.project_id, domain:$ENV.SWIFTDOMAIN, storage_url:$ENV.SWIFTSTORAGEURL }) | to_entries | map("\(.key)=\(.value)") | .[]' >> rclone.config
   
-  echo $VCAP_SERVICES | ./jq '{encryption:env.ENCRYPTION,sources: [ (foreach ."osb-postgresql"[] as $db (1; {name:$db.name,dbtype:"postgres",host:$db.credentials.uri | match("@(.*?):").captures[0].string,database:$db.credentials.database,username:$db.credentials.user, password:$db.credentials.password,keep:$ENV.KEEPBACKUPS }))]}' > sources.json
+  echo $VCAP_SERVICES | ./jq '{encryption:$ENV.ENCRYPTION,sources: [ (foreach ."osb-postgresql"[] as $db (1; {name:$db.name, dbtype:"postgres", host:$db.credentials.uri | match("@(.*?):").captures[0].string, database:$db.credentials.database, username:$db.credentials.user, password:$db.credentials.password, keep:$ENV.KEEPBACKUPS }))]}' > sources.json
 
   ### merge all JSON files to one (expecting exactly two files !)
   ./jq -s '.[0]*.[1]' *.json > config.json
